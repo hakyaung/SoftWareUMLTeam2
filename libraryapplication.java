37bbee75f286d9fce9b862dcc85b08a073835b07
JunseokYU <dbwnstjr03@gmail.com>
2025-11-03 16:21:52 +0900

    1111

---
+++ libraryapplication.java
@@ -0,0 +1,18 @@
+
+/**
+ * libraryapplication 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class libraryapplication
+{
+    public 등록한다(이용자 정보)
+    {
+    
+    }
+    public 추가한다(이용자)
+    {
+    
+    }
+}
