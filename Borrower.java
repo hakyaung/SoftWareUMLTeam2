d9f28f988cca5184b69509515b57e390c213ef3b
정하경 <hk100456@naver.com>
2025-11-26 04:42:48 +0900

    Borrower, BorrowerCollection 수정

--- Borrower.java
+++ Borrower.java
@@ -2,18 +2,48 @@
 /**
  * 이용자 클래스의 설명을 작성하세요.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (유준석, 김민겸, 정하경)
+ * @version (20251126)
  */
 public class Borrower
 {
     private String name;
     private String uniqueNumber;
     private int borrowerLoanStatus;
+    private String adress;
     
-    public Borrower(String name, String uniqueNumber){
+    public Borrower(String name, String uniqueNumber, String adress){
         this.name = name;
         this.uniqueNumber = uniqueNumber;
         this.borrowerLoanStatus = 0;
+        this.adress = adress;
+    }
+    
+    public String getName(){
+        
+    }
+    
+    public String getUniqueNumber(){
+        
+    }
+    
+    public String getAdress(){
+        
+    }
+    
+    public void changeBorrowerLoanStatus(int count){
+        
+    }
+    
+    public boolean checkBookLoanAvility(Borrower borrower){
+        
+    }
+    
+    public int getBorrowerLoanStatus(){
+        
+    }
+    
+    public String toString(){
+        
     }
 }
--- BorrowerCollection.java
+++ BorrowerCollection.java
@@ -1,11 +1,23 @@
-
+import java.util.*;
 /**
  * 이용자DB 클래스의 설명을 작성하세요.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (유준석, 김민겸, 정하경)
+ * @version (20251126)
  */
 public class BorrowerCollection
 {
+    private ArrayList<Borrower> borrowerDB;
     
+    public BorrowerCollection(){
+        this.borrowerDB = new ArrayList<Borrower>();
+    }
+    
+    public void registerToBorrowerDB(Borrower borrower){
+        
+    }
+    
+    public boolean findBorrower(String uniqueNumber){
+        
+    }
 }
