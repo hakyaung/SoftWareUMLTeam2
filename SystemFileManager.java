1098c752ec6ed615f580301271a50647dd62e4e1
정하경 <hk100456@naver.com>
2025-12-02 21:14:58 +0900

    SystemFileManager 수정

--- SystemFileManager.java
+++ SystemFileManager.java
@@ -22,22 +22,52 @@ public class SystemFileManager
         String str = "";
         
         try{
-            FileReader src = new FileReader(borrowerFileSrc);
-            Scanner sc = new Scanner(src);
-            while(sc.hasNext()){
-                str = sc.nextLine();
+            FileReader borrowerSrc = new FileReader(borrowerFileSrc);
+            FileReader bookSrc = new FileReader(bookFileSrc);
+            FileReader loanSrc = new FileReader(loanFileSrc);
+            Scanner borrowerScanner = new Scanner(borrowerSrc);
+            Scanner bookScanner = new Scanner(bookSrc);
+            Scanner loanScanner = new Scanner(loanSrc);
+            while(borrowerScanner.hasNext()){
+                str = borrowerScanner.nextLine();
                 StringTokenizer stz = new StringTokenizer(str, "/");
                 String name = stz.nextToken();
-                String borrowerUniqueNumber = stz.nexToken();
+                String borrowerUniqueNumber = stz.nextToken();
                 String email = stz.nextToken();
                 int loanCount = Integer.valueOf(stz.nextToken());
-                Borrower b = new Borrower(
+                Borrower borrower = new Borrower(name, borrowerUniqueNumber, email, loanCount);
+                borrowerdb.registerToBorrowerDB(borrower);
             }
+            
+            while(bookScanner.hasNext()){
+                str = bookScanner.nextLine();
+                StringTokenizer stz = new StringTokenizer(str, "/");
+                String title = stz.nextToken();
+                String author = stz.nextToken();
+                String bookUniqueNumber = stz.nextToken();
+                Book book = new Book(title, author, bookUniqueNumber);
+                bookdb.registerToBookDB(book);
+            }
+            
+            while(loanScanner.hasNext()){
+                str = loanScanner.nextLine();
+                StringTokenizer stz = new StringTokenizer(str, "/");
+                String borrowerUniqueNumber = stz.nextToken();
+                String bookUniqueNumber = stz.nextToken();
+                Borrower borrower = borrowerdb.getOneBorrower(borrowerUniqueNumber);
+                Book book = bookdb.getOneBook(bookUniqueNumber);
+                Loan loan = new Loan(borrower, book);
+                System.out.println(loandb.registerToLoan(loan));
+            }
+            borrowerSrc.close();
+            bookSrc.close();
+            loanSrc.close();
+            borrowerScanner.close();
+            bookScanner.close();
+            loanScanner.close();
+        }catch(IOException e){
+            return "파일 경로가 올바르지 않습니다.";
         }
+        return "파일 읽기 성공";
     }
 }
-
-private String name;
-    private String borrowerUniqueNumber;
-    private String email;
-    private int loanCount;
