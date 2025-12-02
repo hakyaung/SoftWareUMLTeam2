43eeb737b086c3ebba156ff171cf6cace04eb9c5
정하경 <hk100456@naver.com>
2025-12-03 01:40:31 +0900

    Borrower 클래스 수정

--- Borrower.java
+++ Borrower.java
@@ -49,6 +49,6 @@ public class Borrower
         }
     }
     public String displayBorrower(){
-        return "이용자 이름 : " + name + " 이용자 고유번호 : " + borrowerUniqueNumber + " 이메일 주소 : " + email;
+        return "이용자 이름 : " + name + " 이용자 고유번호 : " + borrowerUniqueNumber + " 이메일 주소 : " + email;
     }
 }
