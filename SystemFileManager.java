ef06ab561d6434ad3a07bc6094573971e1f26054
정하경 <hk100456@naver.com>
2025-12-02 20:17:04 +0900

    SystemFileManager 추가

---
+++ SystemFileManager.java
@@ -0,0 +1,19 @@
+
+/**
+ * SystemFileManager 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class SystemFileManager
+{
+    private BorrowerCollection borrowerdb;
+    private BookCollection bookdb;
+    private LoanCollection loandb;
+    
+    public SystemFileManager(){
+        this.borrowerdb = new BorrowerCollection();
+        this.bookdb = new BookCollection();
+        this.loandb = new LoanCollection();
+    }
+}
